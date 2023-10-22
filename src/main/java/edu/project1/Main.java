4bc92e4e3f5ff25a7d0609aad7e4a49eb99324c0
nuetka <anjalichtin@mail.ru>
2023-10-22 16:36:08 +0300

    Убрано предупреждение

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -10,7 +10,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        List<String> words = Arrays.asList("daisy");
+        List<String> words = Arrays.asList("daisy", "gggg");
         ConsoleHangman game = new ConsoleHangman();
         game.run(words);
     }
